8ab5dca402402ac7ea6ac8790c1dd4129a3540f8
Tejas Dond <tejasdond@yahoo.in>
2016-07-23 16:05:08 +0530

    jxis

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+$dbh1 = mysql_connect('localhost', 'root', ''); 
+$dbh2 = mysql_connect('localhost', 'root', '',true); 
+
+mysql_select_db('test', $dbh1);
+mysql_select_db('news', $dbh2);
+include 'regist.html';
+@$username=$_POST['username'];
+@$password=$_POST['password'];
+@$nick=$_POST['nickname'];
+@$category=$_POST['type'];
+if(isset($username)&&isset($password)){
+	if(!empty($username)&&!empty($password)){
+
+
+		$query="INSERT INTO members values('$username','$password','$nick')";
+		
+	$query_run=mysql_query($query,$dbh1);
+	
+	}
+			
+			
+			
+		
+		
+	
+	
+}
+
+?>
