df18c849dda847a22b06ec268872c184ab753036
Tejas Dond <tejasdond@yahoo.in>
2016-07-23 18:39:59 +0530

    jndc

--- register.php
+++ register.php
@@ -1,19 +1,24 @@
 <?php
-$dbh1 = mysql_connect('localhost', 'root', ''); 
-$dbh2 = mysql_connect('localhost', 'root', '',true); 
+@$dbh1 = mysql_connect('localhost', 'root', ''); 
+@$dbh2 = mysql_connect('localhost', 'root', '',true); 
 
-mysql_select_db('test', $dbh1);
-mysql_select_db('news', $dbh2);
-include 'regist.html';
+@mysql_select_db('team17jp', $dbh1);
+@mysql_select_db('team17jp', $dbh2);
+include 'MentorRegister.html';
 @$username=$_POST['username'];
 @$password=$_POST['password'];
-@$nick=$_POST['nickname'];
-@$category=$_POST['type'];
+@$Technology=$_POST['ct'];
+@$Civil=$_POST['civil'];
+@$gender=$_POST['optradio'];
+@$Mech=$_POST['mech'];
+
+
+	
 if(isset($username)&&isset($password)){
 	if(!empty($username)&&!empty($password)){
 
 
-		$query="INSERT INTO members values('$username','$password','$nick')";
+		$query="INSERT INTO mentors values('','job','$gender','address','city','$Technology','$Civil','1','1','1','1','1','1','87989','3','$password','HDFH','$username')";
 		
 	$query_run=mysql_query($query,$dbh1);
 	
